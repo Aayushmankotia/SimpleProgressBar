69132d661ef80766c9de1b4c799b23c743b59373
Aayush <108062914+Aayushmankotia@users.noreply.github.com>
2024-02-16 12:58:54 +0530

    Update UI index.php

--- index.php
+++ index.php
@@ -6,301 +6,345 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Number Table with Questions</title>
   <style>
-   table {
-  border-collapse: collapse;
-  width: 300px;
-  height: 300px;
-  margin-left: 196px;
-  margin-top: 160px;
-}
+    .main_container {
+      display: flex;
+    width: 66%;
+    justify-content: space-evenly;
+    margin: 214px auto;
+    }
 
-table,
-th,
-td {
-  border: 1px solid black;
-  cursor: pointer;
-}
+    table {
+      border-collapse: collapse;
+      width: 300px;
+      height: 300px;
+      /* margin-left: 196px;
+  margin-top: 230px; */
+    }
 
-th,
-td {
-  padding: 10px;
-  text-align: center;
-  background-color: #e1e1e1;
-}
+    table,
+    th,
+    td {
+      border: 1px solid black;
+      cursor: pointer;
+    }
 
-.clicked {
-  background-color: #0f717e;
-  color: white;
-}
+    th,
+    td {
+      padding: 10px;
+      text-align: center;
+      background-color: #e1e1e1;
+    }
 
-#container {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+    .clicked {
+      background-color: #0f717e;
+      color: white;
+    }
+
+    #container {
   max-width: 400px;
-  margin-top: 20px;
-  margin-left: 40px;
-  position: absolute;
-  top: 188px;
-  left: 36%;
 }
 
-.question-container {
-  width: 100%;
-  margin-bottom: 15px;
-  background-color: #d6e5d6;
-    border: 1px solid #969792;
-  padding: 10px;
-  display: none;
-}
+    .question-container {
+      width: 100%;
+      margin-bottom: 15px;
+      background-color: #d6e5d6;
+      border: 1px solid #969792;
+      padding: 10px;
+      display: none;
+      border-radius: 5px;
+    }
 
-div#questionDivchild {
-  display: flex;
-  justify-content: space-evenly;
-  width: 94%; 
-}
+    div#questionDivchild {
+      display: flex;
+      justify-content: space-evenly;
+      width: 94%;
+    }
 
-.option {
-  width: 128px;
-  background-color: #c9d7c9;
-  padding: 5px;
-}
+    .option {
+      width: 128px;
+      background-color: #c9d7c9;
+      padding: 5px;
+    }
 
-.options {
-  display: flex;
-  gap: 10px;
-  padding-top: 4px;
-}
+    .options {
+      display: flex;
+      gap: 10px;
+      padding-top: 4px;
+    }
 
-.options input[type="radio"] {
-  display: none; 
-}
+    .options input[type="radio"] {
+      display: none;
+    }
 
-/* radio button style */
-.radio-container {
-  position: relative;
-  cursor: pointer;
-  margin-left: 40px;
-}
+    /* radio button style */
+    .radio-container {
+      position: relative;
+      cursor: pointer;
+      margin-left: 40px;
+    }
 
-.radio-container input {
-  position: absolute;
-  opacity: 0;
-  cursor: pointer;
-}
+    .radio-container input {
+      position: absolute;
+      opacity: 0;
+      cursor: pointer;
+    }
 
-.checkmark {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  height: 25px;
-  width: 25px;
-  background-color: #ccc;
-  border-radius: 50%;
-}
+    .checkmark {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      height: 25px;
+      width: 25px;
+      background-color: #ccc;
+      border-radius: 50%;
+    }
 
-.radio-container:hover input ~ .checkmark {
-  background-color: #ddd;
-}
+    .radio-container:hover input~.checkmark {
+      background-color: #ddd;
+    }
 
-.radio-container input:checked ~ .checkmark {
-  background-color: #0F717E; 
-  border: solid #045864 1px;
-}
+    .radio-container input:checked~.checkmark {
+      background-color: #0F717E;
+      border: solid #045864 1px;
+    }
 
-.checkmark:after {
-  content: "";
-  position: absolute;
-  display: none;
-}
+    .checkmark:after {
+      content: "";
+      position: absolute;
+      display: none;
+    }
 
-.radio-container input:checked ~ .checkmark:after {
-  display: block;
-}
+    .radio-container input:checked~.checkmark:after {
+      display: block;
+    }
 
-.radio-container .checkmark:after {
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 7px;
-  height: 7px;
-  border-radius: 50%;
-  background: white;
-}
+    .radio-container .checkmark:after {
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 7px;
+      height: 7px;
+      border-radius: 50%;
+      background: white;
+    }
 
-/* Style for checked text */
-.radio-container input:checked ~ .radio-label {
-  background-color: #0F717E;
-}
+    /* Style for checked text */
+    .radio-container input:checked~.radio-label {
+      background-color: #0F717E;
+    }
 
-.radio-label {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 2px 6px;
-  border-radius: 20px; 
-  color: black;
-}
+    .radio-label {
+      position: absolute;
+      top: 40%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      padding: 2px 6px;
+      border-radius: 20px;
+      color: black;
+    }
 
-#selectedInfo {
-  margin-top: 20px;
-}
+    #selectedInfo {
+      margin-top: 20px;
+    }
 
-.bar_divmain {
-  position: absolute;
-  width: 90%;
-  top: 0px;
-  left: 10%;
-}
+    .bar_divmain {
+      position: absolute;
+      width: 100%;
+      top: 0px;
+      margin: 0 auto;
+    }
 
-.infoscore {
-  height: 35px;
-  width: 160px;
-  padding-top: 15px;
-  padding-left: 30px;
-  color: #383681;
-  font-size: 17px;
-  border: solid black 1px;
-  font-weight: 900;
-  border-radius: 5px;
-}
+    .infoscore {
+      height: 46px;
+      width: 160px;
+      padding-top: 28px;
+      padding-left: 30px;
+      color: #383681;
+      background: #d6e5d6;
+      font-size: 17px;
+      border: solid black 1px;
+      font-weight: 900;
+      border-radius: 5px;
+    }
 
-.infoprogress {
-  margin-top: 20px;
-  display: flex;
-  justify-content: center;
-  gap: 10px;
-}
+    .infoprogress {
+      margin-top: 20px;
+      display: flex;
+      justify-content: center;
+      gap: 10px;
+    }
 
-.barbar {
-  width: 50%;
-  height: 50px;
-  background-color: lightgray;
-  border-radius: 5px;
-  display: flex;
-  overflow: hidden;
-  border: solid #0a0a09 1px;
-}
+    .barbar {
+      width: 50%;
+      height: 74px;
+      background-color: lightgray;
+      border-radius: 5px;
+      display: flex;
+      overflow: hidden;
+      border: solid #0a0a09 1px;
+    }
 
-#progressBardiv {
-  width: 100%;
-  top: 0px;
-  position: absolute;
-  display: flex;
-  justify-content: space-evenly;
-}
+    #progressBardiv {
+      width: 100%;
+      top: 0px;
+      position: absolute;
+      display: flex;
+      justify-content: space-evenly;
+    }
 
-.progress {
-  height: 100%;
-  transition: width 0.5s;
-}
+    .progress {
+      height: 100%;
+      transition: width 0.5s;
+    }
 
-.progress-A {
-  background-color: #3CBB88;
-}
+    .progress-A {
+      background-color: #3CBB88;
+    }
 
-.progress-B {
-  background-color: #8CE1BF;
+    .progress-B {
+      background-color: #8CE1BF;
 
-}
+    }
 
-.progress-C {
-  background-color: #FFE179;
-}
+    .progress-C {
+      background-color: #FFE179;
+    }
 
-.progress-D {
-  background-color: #FFB978;
-}
+    .progress-D {
+      background-color: #FFB978;
+    }
 
-.progress-E {
-  background-color: #ECEAE5;
-}
+    .progress-E {
+      background-color: #ECEAE5;
+    }
 
-.barOption {
-  margin-top: 10px;
-}
-
+    .barOption {
+      margin-top: 10px;
+    }
+    .cover-container{
+      width: 422px;
+    }
   </style>
 </head>
 
 <body>
+  <div class="main_container">
+    <table id="numberTable">
+      <?php
 
-<table id="numberTable">
-  <?php
-  $count = 1;
-  for ($i = 1; $i <= 7; $i++) {
-    echo "<tr>";
-    for ($j = 1; $j <= 7; $j++) {
-      echo "<th onclick='cellClick(this, $i, $j)'>" . $count . "</th>";
-      $count++;
-    }
-    echo "</tr>";
-  }
-  ?>
-</table>
+      // To create a 7*7 table
+      $count = 1;
+      for ($i = 1; $i <= 7; $i++) {
+        echo "<tr>";
+        for ($j = 1; $j <= 7; $j++) {
+          echo "<th onclick='cellClick(this, $i, $j)'>" . $count . "</th>";
 
-<div id="container">
-  <?php
-  $defaultOptions = ['A', 'B', 'C', 'D', 'E'];
-
-  for ($i = 1; $i <= 49; $i++) {
-    $defaultSelection = $defaultOptions[($i - 1) % count($defaultOptions)];
-    echo "<div class='question-container' id='questionDiv{$i}'>";
-    echo "<div class='question-container_child' id='questionDivchild'>";
-    echo "<div class='option'>Question {$i}:<span> $defaultSelection </span></div>";
-    echo "<div class='options'>";
-
-    // Integrate round radio button code here
-    for ($k = 0; $k < 5; $k++) {
-      $optionId = "q{$i}{$k}";
-      $optionValue = chr(65 + $k);
-      echo "<label class='radio-container'>";
-      echo "<input type='radio' id='{$optionId}' name='q{$i}' value='{$optionValue}'";
-      if ($optionValue === $defaultSelection) {
-        echo " checked";
+          // count add the value on each cell of table from 1 to 49
+          $count++;
+        }
+        echo "</tr>";
       }
-      echo ">";
-      echo "<span class='checkmark'></span>";
-      echo "<span class='radio-label'>{$optionValue}</span>";
-      echo "</label>";
-    }
+      
+      ?>
+    </table>
 
-    echo "</div>";
-    echo "</div>";
-    echo "</div>";
-  }
-  ?>
-</div>
+    <div class="cover-container">
+      <div id="container">
+        <?php
+        $defaultOptions = ['A', 'B', 'C', 'D', 'E'];
 
+        for ($i = 1; $i <= 49; $i++) {
+          // To set the default selected option for each question 
+          $defaultSelection = $defaultOptions[($i - 1) % count($defaultOptions)];
+          echo "<div class='question-container' id='questionDiv{$i}'>";
+          echo "<div class='question-container_child' id='questionDivchild'>";
+          echo "<div class='option'>Question {$i}:<span> $defaultSelection </span></div>";
+          echo "<div class='options'>";
 
+          // Integrate round radio button code here
+          for ($k = 0; $k < 5; $k++) {
+            $optionId = "q{$i}{$k}";
+            $optionValue = chr(65 + $k);
+            echo "<label class='radio-container'>";
+            echo "<input type='radio' id='{$optionId}' name='q{$i}' value='{$optionValue}'";
+            if ($optionValue === $defaultSelection) {
+              echo "checked";
+            }
+            echo ">";
+            echo "<span class='checkmark'></span>";
+            echo "<span class='radio-label'>{$optionValue}</span>";
+            echo "</label>";
+          }
+
+          echo "</div>";
+          echo "</div>";
+          echo "</div>";
+        }
+        ?>
+
+      </div>
+    </div>
+  </div>
 
   <div id="selectedInfo"></div>
-<div class="bar_divmain">
-  
 
-  <div id="defaultprogressInfo" class="infoprogress">
-    <div id="defaultScoreInfo" class="infoscore"></div>
-    <div id="defaultprogressBar" class="barbar">
-      <div id="defaultprogressA" class="progress progress-A"><div id="defaultbarOptionA"></div><div id="defaultbarOptioncountA" class="barOption" ></div></div>
-      <div id="defaultprogressB" class="progress progress-B"><div id="defaultbarOptionB"></div><div id="defaultbarOptioncountB" class="barOption" ></div></div>
-      <div id="defaultprogressC" class="progress progress-C"><div id="defaultbarOptionC"></div><div id="defaultbarOptioncountC" class="barOption" ></div></div>
-      <div id="defaultprogressD" class="progress progress-D"><div id="defaultbarOptionD"></div><div id="defaultbarOptioncountD" class="barOption" ></div></div>
-      <div id="defaultprogressE" class="progress progress-E"><div id="defaultbarOptionE"></div><div id="defaultbarOptioncountE" class="barOption" ></div></div>
+  <div class="bar_divmain">
+
+    <div id="defaultprogressInfo" class="infoprogress">
+      <div id="defaultScoreInfo" class="infoscore">Score 2023: 0</div>
+      <div id="defaultprogressBar" class="barbar">
+        <div id="defaultprogressA" class="progress progress-A">
+          <div id="defaultbarOptionA"></div>
+          <div id="defaultbarOptioncountA" class="barOption"></div>
+        </div>
+        <div id="defaultprogressB" class="progress progress-B">
+          <div id="defaultbarOptionB"></div>
+          <div id="defaultbarOptioncountB" class="barOption"></div>
+        </div>
+        <div id="defaultprogressC" class="progress progress-C">
+          <div id="defaultbarOptionC"></div>
+          <div id="defaultbarOptioncountC" class="barOption"></div>
+        </div>
+        <div id="defaultprogressD" class="progress progress-D">
+          <div id="defaultbarOptionD"></div>
+          <div id="defaultbarOptioncountD" class="barOption"></div>
+        </div>
+        <div id="defaultprogressE" class="progress progress-E">
+          <div id="defaultbarOptionE"></div>
+          <div id="defaultbarOptioncountE" class="barOption"></div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div id="progressInfo" class="infoprogress">
-    <div id="scoreInfo" class="infoscore"></div>
-    <div id="progressBar" class="barbar">
-      <div id="progressA" class="progress progress-A"><div id="barOptionA"></div><div id="barOptioncountA" class="barOption" ></div></div>
-      <div id="progressB" class="progress progress-B"><div id="barOptionB"></div><div id="barOptioncountB" class="barOption" ></div></div>
-      <div id="progressC" class="progress progress-C"><div id="barOptionC"></div><div id="barOptioncountC" class="barOption" ></div></div>
-      <div id="progressD" class="progress progress-D"><div id="barOptionD"></div><div id="barOptioncountD" class="barOption" ></div></div>
-      <div id="progressE" class="progress progress-E"><div id="barOptionE"></div><div id="barOptioncountE" class="barOption" ></div></div>
+
+    <div id="progressInfo" class="infoprogress">
+      <div id="scoreInfo" class="infoscore">Score 2024: 0</div>
+      <div id="progressBar" class="barbar">
+        <div id="progressA" class="progress progress-A">
+          <div id="barOptionA"></div>
+          <div id="barOptioncountA" class="barOption"></div>
+        </div>
+        <div id="progressB" class="progress progress-B">
+          <div id="barOptionB"></div>
+          <div id="barOptioncountB" class="barOption"></div>
+        </div>
+        <div id="progressC" class="progress progress-C">
+          <div id="barOptionC"></div>
+          <div id="barOptioncountC" class="barOption"></div>
+        </div>
+        <div id="progressD" class="progress progress-D">
+          <div id="barOptionD"></div>
+          <div id="barOptioncountD" class="barOption"></div>
+        </div>
+        <div id="progressE" class="progress progress-E">
+          <div id="barOptionE"></div>
+          <div id="barOptioncountE" class="barOption"></div>
+        </div>
+      </div>
     </div>
+
   </div>
-</div>
+
   <script>
     function cellClick(cell, row, col) {
       cell.classList.toggle('clicked');
@@ -325,7 +369,6 @@ div#questionDivchild {
     function showSelectedInfo(row, col) {
       var questionNumber = (row - 1) * 7 + col;
       var questionInfo = document.getElementById(`q${questionNumber}0`).parentElement.parentElement.previousElementSibling.innerText;
-
     }
 
     function clearSelectedInfo() {
@@ -339,7 +382,6 @@ div#questionDivchild {
     function unhighlightQuestionDiv(questionDiv) {
       questionDiv.style.display = 'none';
     }
-
 
     function updateScore() {
       var visibleDivs = document.querySelectorAll('.question-container[style="display: block;"]');
@@ -377,7 +419,6 @@ div#questionDivchild {
         }
       });
 
-      
       var totalOptions = visibleDivs.length * 1;
 
       var progressA = (optionCounts['A'] / totalOptions) * 100;
@@ -403,8 +444,21 @@ div#questionDivchild {
       document.getElementById('barOptioncountD').innerHTML = `${optionCounts['D']}`;
       document.getElementById('barOptionE').innerHTML = `E`;
       document.getElementById('barOptioncountE').innerHTML = `${optionCounts['E']}`;
+
+
+      // To remove the bar color on unhighlight all cells 
+      if (totalOptions === 0) {
+        document.querySelectorAll('.progress').forEach(function(progress) {
+          progress.style.display = 'none';
+        });
+      } else {
+        document.querySelectorAll('.progress').forEach(function(progress) {
+          progress.style.display = 'block';
+        });
+      }
     }
 
+    // To display default progress bar, depending upon highlighted cells ,checked radio option and score of each option
     function updateDefaultProgressBar() {
       var visibleDivs = document.querySelectorAll('.question-container[style="display: block;"]');
       var defaultOptionCounts = {
@@ -415,11 +469,12 @@ div#questionDivchild {
         'E': 0
       };
 
+      // To get the total of option checked
       visibleDivs.forEach(function(div) {
         var selectedOption = div.querySelector('input[type="radio"][checked]');
         if (selectedOption) {
           var optionValue = selectedOption.value;
-          defaultOptionCounts[optionValue]++;
+          defaultOptionCounts[optionValue]++; // no idea
         }
       });
 
@@ -449,16 +504,16 @@ div#questionDivchild {
       document.getElementById('defaultbarOptioncountD').innerHTML = `${defaultOptionCounts['D']}`;
       document.getElementById('defaultbarOptionE').innerHTML = `E`;
       document.getElementById('defaultbarOptioncountE').innerHTML = `${defaultOptionCounts['E']}`;
-  
+
       // Calculate and display default score
-        var totalDefaultScore = 0;
+      var totalDefaultScore = 0;
       for (var option in defaultOptionCounts) {
         totalDefaultScore += getOptionScore(option) * defaultOptionCounts[option];
       }
       var defaultAverageScore = totalOptions > 0 ? totalDefaultScore / totalOptions : 0;
       document.getElementById('defaultScoreInfo').innerHTML = `Score 2021: ${Math.round(defaultAverageScore)}`;
     }
-  
+
     function getOptionScore(optionValue) {
       switch (optionValue) {
         case 'A':
